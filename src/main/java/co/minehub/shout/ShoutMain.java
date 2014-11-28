3e3c05dc683c39fb2b1fa5d9b6c3993a5be18a35
Wferr <info@wferr.com>
2014-11-27 20:13:43 -0800

    Edits

--- src/main/java/co/minehub/shout/ShoutMain.java
+++ src/main/java/co/minehub/shout/ShoutMain.java
@@ -1,4 +1,4 @@
-package nhadobas.net.spigot.chat;
+package co.minehub.shout;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -8,16 +8,16 @@ import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 
-public class NetMain extends Plugin
+public class ShoutMain extends Plugin
         implements Listener {
 
     public void onEnable() {
-        getProxy().getLogger().log(Level.INFO, "NetChat may or may not enable!");
-        new NetChat(this);
-        getProxy().getLogger().log(Level.INFO, "NetChat is enabled!");
+        getProxy().getLogger().log(Level.INFO, "Shout may or may not enable!");
+        new Shout(this);
+        getProxy().getLogger().log(Level.INFO, "Shout is enabled!");
     }
 
     public void onDisable() {
-        getProxy().getLogger().log(Level.INFO, "NetChat is disabling!");
+        getProxy().getLogger().log(Level.INFO, "Shout is disabling!");
     }
 }
